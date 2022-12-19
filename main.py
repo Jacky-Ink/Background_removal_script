e589614c3212edbb64dcfe3987ac42fdaba56b25
Инна Кляхина <inna.galkina95@ya.ru>
2022-12-19 15:38:27 +0300

    v1.1

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+from rembg import remove
 from PIL import Image
 from pathlib import Path
 
@@ -10,7 +10,20 @@ def remove_bg():
     for ext in list_of_extension:
         all_files.extend(Path('C:\ProjectsPython\Background_removal_script\input_imgs').glob(ext))
 
-    print(all_files)
+    # print(all_files)
+
+    for index, item in enumerate(all_files):
+        input_path = Path(item)
+        file_name = input_path.stem
+
+        output_path = f'C:\ProjectsPython\Background_removal_script\output_imgs/{file_name}_output.png'
+        # print(output_path)
+
+        input_img = Image.open(input_path)
+        output_img = remove(input_img)
+        output_img.save(output_path)
+
+        print(f'Completed: {index + 1}/{len(all_files)}')
 
 
 def main():
